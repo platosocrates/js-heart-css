472a90358f9dc10ffd58717530e710ee36309eca
Lee <liam.eakins@outlook.com>
2018-12-25 04:52:04 -0600

    Complete project

---
+++ js/main.js
@@ -0,0 +1,23 @@
+$('header').addClass('masthead');
+
+$('h1').removeClass('big-heading').addClass('logo');
+
+$('main').addClass('wrapper');
+
+$('main p:first-child').addClass('intro');
+
+$('h2').addClass('slug-head');
+
+$('main ul').addClass('slug-list');
+
+$('img').addClass('slug-img');
+
+$('ul li:first-child').addClass('slug-list-first');
+
+$('ul li:last-child').addClass('slug-list-last');
+
+$('dl').addClass('classification');
+
+$('dt').removeClass('big-label');
+
+$('ul li:last-child').show();
